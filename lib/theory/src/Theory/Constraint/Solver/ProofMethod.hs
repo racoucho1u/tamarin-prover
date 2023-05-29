6efbfc2debdcfc0e1247f2983f16b3422046c9bc
mracouch <maiwenn.racouchot@inria.fr>
2023-05-29 12:57:08 +0200

    typo

--- lib/theory/src/Theory/Constraint/Solver/ProofMethod.hs
+++ lib/theory/src/Theory/Constraint/Solver/ProofMethod.hs
@@ -1225,7 +1225,7 @@ prettyProofMethod :: HighlightDocument d => ProofMethod -> d
 prettyProofMethod method = case method of
     Solved               -> keyword_ "SOLVED" <-> lineComment_ "trace found"
     Induction            -> keyword_ "induction"
-    InLoop (idx, goal)   -> fsep [keyword_ "solve(" <-> prettyGoal goal <-> keyword_ ")", maybe emptyDoc closedComment_ (Just $ "inLoop "++show i)]
+    InLoop (idx, goal)   -> fsep [keyword_ "solve(" <-> prettyGoal goal <-> keyword_ ")", maybe emptyDoc closedComment_ (Just $ "inLoop "++show idx)]
     BlackListed goal     -> fsep [keyword_ "solve(" <-> prettyGoal goal <-> keyword_ ")", maybe emptyDoc closedComment_ (Just "blacklisted goal")]
     Sorry reason         ->
         fsep [keyword_ "sorry", maybe emptyDoc closedComment_ reason]
