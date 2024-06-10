34483658efcc58f17af2e93deca8494c1757b165
mracouch <maiwenn.racouchot@inria.fr>
2024-06-10 16:05:18 +0200

    Srv ready

--- lib/theory/src/Theory/Proof.hs
+++ lib/theory/src/Theory/Proof.hs
@@ -1053,15 +1053,15 @@ proveSystemDFS heuristic tactics ctxt d0 sys0 =
           []                             -> node Unfinishable M.empty
           ((method, (cases, _expl)):suite) -> checkForLoop ((method, (cases, _expl)):suite) (method, (cases, _expl))
       where
-        exportTactic :: String -> ProofMethod -> M.Map CaseName System -> Proof ()
+        {-exportTactic :: String -> ProofMethod -> M.Map CaseName System -> Proof ()
         exportTactic tacticFile method cases = unsafePerformIO $ do
           writeFile tacticFile (show method)
-          return (node method cases)
+          return (node method cases)-}
 
         checkForLoop :: [(ProofMethod, (M.Map CaseName System, String))] -> (ProofMethod, (M.Map CaseName System, String)) -> Proof ()
         --checkForLoop [] = node (InLoop (0,method)) M.empty
         --Change in the case no more option, instead of leaving, pushing through the last option: needs to be tested independently
-        checkForLoop [] (method0, (cases0, _expl0)) = exportTactic generatedTactic method0 cases0
+        checkForLoop [] (method0, (cases0, _expl0)) = node method0 cases0--exportTactic generatedTactic method0 cases0
         checkForLoop ((method, (cases, _expl)):suite) (method0, (cases0, _expl0)) = case method of 
             InLoop _ -> checkForLoop suite (method0, (cases0, _expl0))
             _ -> node method cases
